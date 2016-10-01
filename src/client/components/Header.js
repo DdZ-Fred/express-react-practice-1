ec0a94618bbd2819f998461b02cad53c05ee0df4
Frederic Rey <Frederic.Rey.Pro@gmail.com>
2016-10-01 15:58:16 +0200

    - UI Code molularized using ReactPureComponent (TopNavMenu, Header, SideBar, Footer)

---
+++ src/client/components/Header.js
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react';
+
+class Header extends PureComponent {
+  render() {
+    return (
+      <div className="blog-header">
+          <div className="container">
+              <h1 className="blog-title">The Bootstrap Blog</h1>
+              <p className="lead blog-description">
+                  An example blog template built with Bootstrap.
+              </p>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
